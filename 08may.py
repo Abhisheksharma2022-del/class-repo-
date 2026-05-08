a2c92763c8a2aa0474913ee1f1240f2ade6d8dbb
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-08 10:18:46 +0530

    08may.py

---
+++ 08may.py
@@ -0,0 +1,27 @@
+# Loops in python
+# 1. for loop : ranged based 
+# 2. while Loop : condition based 
+
+
+# 1. for loop 
+#print("Aman")
+# range always works with integer numbers only 
+# range (start :0, stop-1,step:1)
+#for i in range(1,6):
+ # print(i)
+#for i in range (1,15,2):
+    #print (i)
+#for i in range (1,10,1):
+
+
+#for i in range(1, 21):
+    #if i % 2 == 0:
+        #print(i, end=" ")
+
+for r in range (1,15,1):
+    print(r)
+    
+        
+   
+        
+      
