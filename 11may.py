3e9f840cf31bc6bc81669e7d4bd75d2d06d5c15c
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-11 10:33:29 +0530

    11may.py

---
+++ 11may.py
@@ -0,0 +1,12 @@
+# Nested for Loop 
+# for i in range (1,6):
+#     print ("First Loop ")
+# for i in range (1,6):
+#     print (i, "First loop ")
+#     for j in range (1,6):
+#         print ("Inner Loop ")
+
+# emp_name=["abhi","shivam","anshu"]
+# for i in emp_name:
+#     print(i)
+
