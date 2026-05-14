08125189419219607a784fb8e6b4f62aefba0913
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-14 10:11:08 +0530

    address

--- 14may.py
+++ 14may.py
@@ -1,9 +1,21 @@
-text="This is python"
+# text="This is python"
 
-space_cont= 0
-for ch in text:
-    if ch == " ":
+# space_cont= 0
+# for ch in text:
+#     if ch == " ":
 
-        space_cont += 1
+#         space_cont += 1
 
-print("Total spaces =", space_cont)
+# print("Total spaces =", space_cont)
+
+
+str="D-1 267/268 MAYUR-VIHAR-PHASE-3 110096"
+num="0123456789"
+c=0
+for i in str :
+    if i in num:
+        c+=1
+print(c)
+
+
+
