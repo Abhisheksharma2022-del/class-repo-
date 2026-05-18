87b55df0f44b2f7468017a3e96fc7afbddae1911
abhi.py <abhibhardwaj7271@gmail.com>
2026-05-18 10:32:55 +0530

    Whilrloop

---
+++ 18may.py
@@ -0,0 +1,17 @@
+# wap to count total number of vowels in a string using while loop .
+
+# str1 = "abhi"
+# size=len(str1) -1
+# while size >=0:
+#     print(size)
+#     size-=1
+
+# wap to print formated table on a number given by user . 
+
+a=int(input("Enter you number :::"))
+n=1
+
+while n<=10:
+    b=a*n
+    print(f"{a}*{n}={b}")
+    n+=1
